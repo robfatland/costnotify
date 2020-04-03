e6a1bc37a0b25eb3ae2dd7e33d8bfe78059842f1
Rob Fatland <rob5@uw.edu>
2020-04-03 07:26:29 -0700

    rob

--- costnotify.py
+++ costnotify.py
@@ -206,30 +206,3 @@ def lambda_handler(event, context):
         print(e)
         print('Error getting object {} from bucket {}'.format(key, bucketname))
         raise e
-        
-        
-#######################
-###
-### Artifact code from the past
-###
-#######################
-
-### choose which file(s) to parse
-# def FileChoice(contents_list):
-    # establish two lists: filenames and the time that each was last updated (seconds since 1970!)
-    # filelist, updateTime = [], []
-    # for element in contents_list:
-        # filename = element['Key'].split('.')    # a printable 3-element list: <long-filename>, '.csv', '.zip'
-        # filetime = element['LastModified']
-        # if filename[-1] == 'zip':
-            # filelist.append(element['Key'])
-            # updateTime.append(element['LastModified'].timestamp())     # Verified: len(filelist) is equal to len(updateTime)
-
-    # this could be expanded to a list of files if we are at a month or year boundary right now
-    # return filelist[-1]
-    
-# flag this will need attention on month/year boundaries
-# fileChosen = FileChoice(csv_file_list['Contents'])
-
-# For reference: some_dt = datetime.datetime(x.year, x.month, x.day, 0, 0, 0);
-# reference components of the datetime using for example some_datetime.year
